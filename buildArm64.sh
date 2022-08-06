3f8c48828617fd8fcd5be24e9c8ff304b407a927
Richard Raumberger <richard@raumberger.net>
2022-08-06 22:27:04 +0200

    Update buildArm64.sh

--- buildArm64.sh
+++ buildArm64.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source ./seed.env
-sudo docker buildx build --platform linux/arm64 -t ${IMAGE_NAME}:${DYNATRACE_AG_VERSION} --push . \
+docker buildx build --platform linux/arm64 -t ${IMAGE_NAME}:${DYNATRACE_AG_VERSION} --push . \
   --build-arg DYNATRACE_ENVIRONMENT_URL=${DYNATRACE_ENVIRONMENT_URL} \
   --build-arg DYNATRACE_PAAS_TOKEN=${DYNATRACE_PAAS_TOKEN} \
   --build-arg DYNATRACE_AG_VERSION=${DYNATRACE_AG_VERSION}
