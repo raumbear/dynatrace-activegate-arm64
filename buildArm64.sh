2e1176439670fbf1c60b296e16a4b74549738ad9
rraumberger <richard@raumberger.net>
2023-04-20 10:41:28 +0200

    remove source seed

--- buildArm64.sh
+++ buildArm64.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-source seed.env
 docker buildx build --platform linux/arm64 -t ${IMAGE_NAME}:${DYNATRACE_AG_VERSION} --push . \
   --build-arg DYNATRACE_ENVIRONMENT_URL=${DYNATRACE_ENVIRONMENT_URL} \
   --build-arg DYNATRACE_PAAS_TOKEN=${DYNATRACE_PAAS_TOKEN} \
