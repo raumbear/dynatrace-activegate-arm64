078f0a8abc3cf13bf8e7fb66ae74c64e828a8188
rraumberger <richard@raumberger.net>
2023-04-20 10:36:45 +0200

    fix source command

--- buildArm64.sh
+++ buildArm64.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source ./seed.env
+source seed.env
 docker buildx build --platform linux/arm64 -t ${IMAGE_NAME}:${DYNATRACE_AG_VERSION} --push . \
   --build-arg DYNATRACE_ENVIRONMENT_URL=${DYNATRACE_ENVIRONMENT_URL} \
   --build-arg DYNATRACE_PAAS_TOKEN=${DYNATRACE_PAAS_TOKEN} \
