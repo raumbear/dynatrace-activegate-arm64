cb8eb595806c0f36cadb59056828f520d2028218
rraumberger <richard@raumberger.net>
2023-04-20 11:32:57 +0200

    update ag log manager

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,4 @@ sed -i "s/AG_GROUP_PLACEHOLDER/${DYNATRACE_AG_GROUP}/" /ag/config/config/custom.
 sed -i "s/ENTRYPOINT_PLACEHOLDER/${DYNATRACE_AG_ENTRYPOINT}/" /ag/config/config/custom.properties
 
 # Run Activegate
-java -classpath "/ag/install/lib/*" ${DYNATRACE_AG_JVM_ARGS} -XX:ErrorFile=/var/log/dynatrace/gateway/hs_err_pid_%p.log -Duser.language=en -Djava.util.logging.manager=com.compuware.apm.logging.impl.backend.CustomShutdownLogManager -Djdk.tls.ephemeralDHKeySize=2048 -Dorg.xerial.snappy.lib.path=/ag/install/lib/native -Dorg.xerial.snappy.lib.name=libsnappyjava.so com.compuware.apm.collector.core.CollectorImpl -CONFIG_DIR /ag/config/config
+java -classpath "/ag/install/lib/*" ${DYNATRACE_AG_JVM_ARGS} -XX:ErrorFile=/var/log/dynatrace/gateway/hs_err_pid_%p.log -Duser.language=en -Djava.util.logging.manager=com.dynatrace.gen2.foundation.logging.impl.backend.CustomShutdownLogManager -Djdk.tls.ephemeralDHKeySize=2048 -Dorg.xerial.snappy.lib.path=/ag/install/lib/native -Dorg.xerial.snappy.lib.name=libsnappyjava.so com.compuware.apm.collector.core.CollectorImpl -CONFIG_DIR /ag/config/config
